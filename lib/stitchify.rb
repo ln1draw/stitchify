0cada1ab6fb219bcd4dfb4942ce213c4321755dc
ellen wondra <ellenfromillinois@gmail.com>
2018-04-15 13:55:00 -0700

    adds stitch codes for first 17 figures

--- lib/stitchify.rb
+++ lib/stitchify.rb
@@ -3,128 +3,221 @@ class Stitchify
     require 'open-uri'
     require 'pry'
     require 'rasem'
+    require 'asciiart'
 
-    attr_accessor :title, :links, :left, :px, :links_array, :left_pos, :width, :height
-
-	def self.google
-        self.stitch("http://www.google.com")
-	end
-
-    def self.stitch(content)
-        stitcher = Stitchify.new()
-
-        doc = stitcher.get_doc(content)
-        stitcher.title = doc.title
-        stitcher.links = doc.css('a')
-        stitcher.px = 40
-        stitcher.links_array = []
-        stitcher.left_pos = stitcher.px
-        stitcher.width = stitcher.px * 10
-        stitcher.height = stitcher.px * 10
-
-        stitcher.build_links_array
-
-        stitcher
+    def arr_builder(img)
+        AsciiArt.new(img).to_ascii_art.split("\n")
     end
 
-    def get_doc(content)
-        ret = nil
-        if content.strip[0] == '<'
-            ret = Nokogiri::HTML(content)
-        else
-            ret = Nokogiri::HTML(open(content))
+    def char_builder(char)
+        output = []
+        case char
+        when '.'
+            output << pos_slope_one(15, 35, 10)
+            output << neg_slope_one(15, 25, 10)
+        when '~'
+            output << pos_slope_one(5, 25, 10)
+            output << neg_slope_one(15, 15, 10)
+            output << pos_slope_one(25, 25, 10)
+        when ':'
+            output << pos_slope_one(15, 35, 10)
+            output << neg_slope_one(15, 25, 10)
+            output << pos_slope_one(15, 15, 10)
+            output << neg_slope_one(15, 5, 10)
+        when '+'
+            output << vertical_line(25, 5, 30)
+            output << horizontal_line(5, 15, 30)
+        when '='
+            output << horizontal_line(5, 15, 30)
+            output << horizontal_line(5, 25, 30)
+        when 'o'
+            output << pos_slope_one(5, 15, 10)
+            output << horizontal_line(15, 5, 10)
+            output << neg_slope_one(25, 5, 10)
+            output << vertical_line(35, 15, 10)
+            output << pos_slope_one(25, 35, 10)
+            output << horizontal_line(15, 35, 10)
+            output << neg_slope_one(5, 25, 10)
+            output << vertical_line(5, 15, 10)
+        when '*'
+            output << pos_slope_one(5, 35, 30)
+            output << neg_slope_one(5, 5, 30)
+            output << vertical_line(25, 5, 30)
+        when 'x'
+            output << pos_slope_one(5, 35, 30)
+            output << neg_slope_one(5, 5, 30)
+        when '^'
+            output << pos_slope_one(15, 15, 10)
+            output << neg_slope_one(25, 5, 10)
+        when '%'
+            output << pos_slope_one(5, 15, 10)
+            output << neg_slope_one(5, 5, 10)
+            output << pos_slope_one(5, 35, 30)
+            output << pos_slope_one(25, 35, 10)
+            output << neg_slope_one(25, 25, 10)
+        when '#'
+            output << vertical_line(15, 5, 30)
+            output << vertical_line(25, 5, 30)
+            output << horizontal_line(5, 15, 30)
+            output << horizontal_line(5, 25, 30)
+        when '@'
+            output << pos_slope_one(25, 25, 10)
+            output << neg_slope_one(25, 15, 10)
+            output << vertical_line(35, 5, 20)
+            output << horizontal_line(15, 5, 20)
+            output << pos_slope_two(5, 25, 10)
+            output << neg_slope_one(5, 25, 10)
+            output << horizontal_line(15, 35, 20)
+        when '$'
+            output << pos_slope_one(15, 15, 10)
+            output << neg_slope_one(25, 5, 10)
+            output << neg_slope_half(15, 15, 10)
+            output << pos_slope_one(25, 35, 10)
+            output << neg_slope_one(15, 25, 10)
+            output << vertical_line(25, 5, 30)
+        when 'M'
+            output << vertical_line(15, 5, 30)
+            output << neg_slope_two(15, 5, 10)
+            output << pos_slope_two(25, 25, 10)
+            output << vertical_line(35, 5, 30)
+        when 'W'
+            output << vertical_line(15, 5, 30)
+            output << pos_slope_two(15, 35, 10)
+            output << neg_slope_two(25, 15, 10)
+            output << vertical_line(35, 5, 30)
+        when '|'
+            output << vertical_line(25, 5, 30)
+        when '_'
+            output << horizontal_line(5, 35, 30)
         end
-        ret
+        output
     end
 
-    def build_links_array
-        self.left_pos
-
-        px = self.px
-        left_pos = self.px
-        bottom_pos = self.px * 3
-
-        arr = self.links.map{ | x | x.text.strip }
-        arr.each do |word|
-            unless word.length == 0
-                word.split('').each do | letter |
-                    add_letter(letter, left_pos, bottom_pos)
-
-                    right_one_px
-                end
-
-                right_one_px
-                right_one_px
-            end
-        end
-
-        self.links_array
+    def pos_slope_one(startX, startY, length)
+        [startX, startY, startX + length, startY - length]
     end
 
-    def build_svg
-        links_array = self.links_array
-        Rasem::SVGImage.new(width: self.width, height: self.height) do
-            for line_data in links_array
-                line line_data[0], line_data[1], line_data[2], line_data[3]
-            end
-        end
+    def neg_slope_one(startX, startY, length)
+        [startX, startY, startX + length, startY + length]
     end
 
-    def add_letter(letter, left_pos, bottom_pos)
-        left_pos = self.left_pos
-        case letter
-        when ' '
-            right_one_px
-        when 'A'
-            self.links_array << [ left_pos,      bottom_pos,          left_pos,      bottom_pos - 2 * px ]
-            self.links_array << [ left_pos,      bottom_pos - px,     left_pos + px, bottom_pos - px     ]
-            self.links_array << [ left_pos,      bottom_pos - 2 * px, left_pos + px, bottom_pos - 2 * px ]
-            self.links_array << [ left_pos + px, bottom_pos,          left_pos + px, bottom_pos - 2 * px ]
-        end
+    def vertical_line(startX, startY, length)
+        [startX, startY, startX, startY + length]
     end
 
-    def right_one_px
-        self.left_pos = self.left_pos + self.px
+    def horizontal_line(startX, startY, length)
+        [startX, startY, startX + length, startY]
     end
 
-    def make_img
-        Rasem::SVGImage.new(width: 400, height: 400) do
-
-        end
+    def pos_slope_two(startX, startY, width)
+        [startX, startY, startX + width, startY - (2 * width)]
     end
 
-    def write_file
-        File.open("stitchify.svg", "w") do |f|
-            self.make_img.write(f)
-        end
+    def neg_slope_half(startX, startY, height)
+        [startX, startY, startX + (2 * height), startY + height]
     end
 
-    def capital_a
-        line x, x, x, f
-        line h, x, h, f
-        line x, x, h, x
-        line x, h, h, h
+    def neg_slope_two(startX, startY, width)
+        [startX, startY, startX + width, startY + (2 * width)]
     end
-
-    def lower_a
-        line x, h, h, h
-        line h, h, h, f
-        line h, f, x, f
-        line x, l, h, l
-        line x, l, x, f
-    end
-
-    def capital_b
-        line x, x, t, x
-        line x, x, x, f
-        line x, f, h, f
-        line h, f, h, h
-        line h, h, x, h
-        line t, x, t, h
-    end
-
-
 end
+
+ #    attr_accessor :title, :links, :left, :px, :links_array, :left_pos, :width, :height
+
+	# def self.google
+ #        self.stitch("http://www.google.com")
+	# end
+
+ #    def self.stitch(content)
+ #        stitcher = Stitchify.new()
+
+ #        doc = stitcher.get_doc(content)
+ #        stitcher.title = doc.title
+ #        stitcher.links = doc.css('a')
+ #        stitcher.px = 40
+ #        stitcher.links_array = []
+ #        stitcher.left_pos = stitcher.px
+ #        stitcher.width = stitcher.px * 10
+ #        stitcher.height = stitcher.px * 10
+
+ #        stitcher.build_links_array
+
+ #        stitcher
+ #    end
+
+ #    def get_doc(content)
+ #        ret = nil
+ #        if content.strip[0] == '<'
+ #            ret = Nokogiri::HTML(content, nil, 'UTF-8')
+ #        else
+ #            ret = Nokogiri::HTML(open(content), nil, 'UTF-8')
+ #        end
+ #        ret
+ #    end
+
+ #    def build_links_array
+ #        self.left_pos
+
+ #        px = self.px
+ #        left_pos = self.px
+ #        bottom_pos = self.px * 3
+
+ #        arr = self.links.map{ | x | x.text.strip }
+ #        arr.each do |word|
+ #            unless word.length == 0
+ #                word.split('').each do | letter |
+ #                    puts letter
+ #                    add_letter(letter, left_pos, bottom_pos)
+
+ #                    right_one_px
+ #                end
+
+ #                right_one_px
+ #                right_one_px
+ #            end
+ #        end
+
+ #        self.links_array
+ #    end
+
+ #    def build_svg
+ #        links_array = self.links_array
+ #        Rasem::SVGImage.new(width: self.width, height: self.height) do
+ #            for line_data in links_array
+ #                line line_data[0], line_data[1], line_data[2], line_data[3]
+ #            end
+ #        end
+ #    end
+
+ #    def add_letter(letter, left_pos, bottom_pos)
+ #        left_pos = self.left_pos
+ #        case letter
+ #        when ' '
+ #            right_one_px
+ #        when 'A'
+ #            self.links_array << [ left_pos,      bottom_pos,          left_pos,      bottom_pos - 2 * px ]
+ #            self.links_array << [ left_pos,      bottom_pos - px,     left_pos + px, bottom_pos - px     ]
+ #            self.links_array << [ left_pos,      bottom_pos - 2 * px, left_pos + px, bottom_pos - 2 * px ]
+ #            self.links_array << [ left_pos + px, bottom_pos,          left_pos + px, bottom_pos - 2 * px ]
+ #        end
+ #    end
+
+ #    def right_one_px
+ #        self.left_pos = self.left_pos + self.px
+ #    end
+
+ #    def make_img
+ #        Rasem::SVGImage.new(width: 400, height: 400) do
+
+ #        end
+ #    end
+
+ #    def write_file
+ #        File.open("stitchify.svg", "w") do |f|
+ #            self.make_img.write(f)
+ #        end
+ #    end
+
 
 
 =begin
@@ -134,6 +227,7 @@ end
 
 
 
+15, 25, 25, 35
 
 
 takes position and letter
@@ -149,7 +243,11 @@ takes position and letter
 2.4.1 :008 >     img.write(f)
 2.4.1 :009?>   end
 
-
+def stitch(img)
+    File.open("stitchify.svg", "w") do |f|
+        img.write(f)
+    end
+end
 stitcher.links[5].text
     
 =end
--- spec/stitchify_spec.rb
+++ spec/stitchify_spec.rb
@@ -1,69 +1,214 @@
 require 'spec_helper'
 
 describe "Stitchify" do
-    describe 'instance method' do
-        describe 'get_doc' do
-            it 'returns a Nokogiri doc object when given a valid html string' do
-                stitcher = Stitchify.new()
-                ret = stitcher.get_doc("<html><body><h1>Hello</h1></body></html>")
-                expect(ret.class).to be(Nokogiri::HTML::Document)
-            end
+    describe 'arr_builder' do
+        it 'takes an image and returns an array of characters' do
+            s = Stitchify.new
+            expect(s.arr_builder('logo.png').class).to eq(Array)
+        end
+    end
 
-            it 'returns a Nokogiri doc object when given a valid link' do
-                stitcher = Stitchify.new()
-                ret = stitcher.get_doc("http://www.google.com")
-            end
+    describe 'line methods' do
+        it 'pos_slope_one gives expected vals' do
+            s = Stitchify.new
+            expect(s.pos_slope_one(15, 35, 10)).to eq([15, 35, 25, 25])
+            expect(s.pos_slope_one(15, 35, 15)).to eq([15, 35, 30, 20])
         end
 
-        describe 'build_links_array' do
+        it 'neg_slope_one gives expected vals' do
+            s = Stitchify.new
+            expect(s.neg_slope_one(15, 35, 10)).to eq([15, 35, 25, 45])
+            expect(s.neg_slope_one(15, 35, 15)).to eq([15, 35, 30, 50])
+        end
+
+        it 'vertical_line' do
+            s = Stitchify.new
+            expect(s.vertical_line(25, 5, 30)).to eq([25, 5, 25, 35])
+            expect(s.vertical_line(25, 5, 15)).to eq([25, 5, 25, 20])
+        end
+
+        it 'horizontal_line' do
+            s = Stitchify.new
+            expect(s.horizontal_line(5, 15, 30)).to eq([5, 15, 35, 15])
+        end
+    end
+    # .~:+=o*x^%#@$MW
+
+    describe 'char_builder' do
+        describe 'takes letters and returns arrays of numbers' do
             before(:all) do
-                @html_str = "<html><body><a href='example.com'>A</a></body></html>"
+                @s = Stitchify.new
             end
 
-            it 'returns an array of arrays' do
-                stitcher = Stitchify.stitch(@html_str)
-                ret = stitcher.get_doc(@html_str)
-                stitcher.build_links_array
-                expect(stitcher.links_array.class).to eq(Array)
-                expect(stitcher.links_array[0].class).to eq(Array)
+            it 'takes .' do
+                expect(@s.char_builder('.')).to eq([[15, 35, 25, 25], [15, 25, 25, 35]])
+            end
+            
+            it 'takes ~' do
+                expect(@s.char_builder('~')).to eq([[5, 25, 15, 15], [15, 15, 25, 25], [25, 25, 35, 15]])
+            end
+            
+            it 'takes :' do
+                expect(@s.char_builder(':')).to eq(  [[15, 35, 25, 25], [15, 25, 25, 35], [15, 15, 25, 5], [15, 5, 25, 15]])
             end
 
-            it 'returns A' do
-                stitcher = Stitchify.stitch("<html><body><a href='example.com'>A A</a></body></html>")
-                ret = stitcher.get_doc(@html_str)
-                stitcher.build_links_array
-                expect(stitcher.links_array).to eq([[40, 120, 40, 40], [40, 80, 80, 80], [40, 40, 80, 40], [80, 120, 80, 40], [160, 120, 160, 40], [160, 80, 200, 80], [160, 40, 200, 40], [200, 120, 200, 40]])
+            it 'takes +' do
+                expect(@s.char_builder('+')).to eq([[25, 5, 25, 35], [5, 15, 35, 15]])
             end
+
+            it 'takes =' do
+                expect(@s.char_builder('=')).to eq([[5, 15, 35, 15], [5, 25, 35, 25]])
+            end
+
+            it 'takes o' do
+                expect(@s.char_builder('o')).to eq([[5,  15, 15, 5],
+                                                    [15, 5,  25, 5],
+                                                    [25, 5,  35, 15],
+                                                    [35, 15, 35, 25],
+                                                    [25, 35, 35, 25],
+                                                    [15, 35, 25, 35],
+                                                    [5,  25, 15, 35],
+                                                    [5,  15, 5,  25]])
+            end
+
+            it 'takes *' do
+                expect(@s.char_builder('*')).to eq([[5, 35, 35, 5], [5, 5, 35, 35], [25, 5, 25, 35]])
+            end
+
+            it 'takes x' do
+                expect(@s.char_builder('x')).to eq([[5, 35, 35, 5], [5, 5, 35, 35]])
+            end
+
+            it 'takes ^' do
+                expect(@s.char_builder('^')).to eq([[15, 15, 25, 5], [25, 5, 35, 15]])
+            end
+
+            it 'takes %' do
+                expect(@s.char_builder('%')).to eq([[5, 15, 15, 5], [5, 5, 15, 15], [5, 35, 35, 5], [25, 35, 35, 25], [25, 25, 35, 35]])
+            end
+
+            it 'takes #' do
+                expect(@s.char_builder('#')).to eq([[15, 5, 15, 35], [25, 5, 25, 35], [5, 15, 35, 15], [5, 25, 35, 25]])
+            end
+
+            it 'takes @' do
+                expect(@s.char_builder('@')).to eq([[25, 25, 35, 15],
+                                                    [25, 15, 35, 25],
+                                                    [35,  5, 35, 25],
+                                                    [15,  5, 35,  5],
+                                                    [5,  25, 15,  5],
+                                                    [5,  25, 15, 35],
+                                                    [15, 35, 35, 35]])
+            end
+
+            it 'takes $' do
+                expect(@s.char_builder('$')).to eq([[15, 15, 25,  5],
+                                                    [25,  5, 35, 15],
+                                                    [15, 15, 35, 25],
+                                                    [25, 35, 35, 25],
+                                                    [15, 25, 25, 35],
+                                                    [25,  5, 25, 35]])
+            end
+
+            it 'takes M' do
+                expect(@s.char_builder('M')).to eq([[15, 5, 15, 35], [15, 5, 25, 25], [25, 25, 35, 5], [35, 5, 35, 35]])
+            end
+
+            it 'takes W' do
+                expect(@s.char_builder('W')).to eq([[15, 5, 15, 35], [15, 35, 25, 15], [25, 15, 35, 35], [35, 5, 35, 35]])
+            end
+
+            it 'takes |' do
+                expect(@s.char_builder('|')).to eq([[25, 5, 25, 35]])
+            end
+
+            it 'takes _' do
+                expect(@s.char_builder('_')).to eq([[5, 35, 35, 35]])
+            end
+
         end
-
-        describe 'build_svg' do
-            before(:each) do
-                @html_str = "<html><body><a href='example.com'>A</a></body></html>"
-                @stitcher = Stitchify.stitch(@html_str)
-                @stitcher.build_links_array
-            end
-
-            it 'returns an svg' do
-                ret = @stitcher.build_svg
-                expect(ret.class).to eq(Rasem::SVGImage)
-            end
+        it 'can be changed by changing the pixel size' do
+        end
+        it 'can take multiple characters' do
         end
     end
 
-    describe 'class method' do
-        describe 'stitch' do
-            it 'returns a set stitcher' do
-                stitcher = Stitchify.stitch('http://www.google.com')
-                expect(stitcher.title).to eq('Google')
-            end
-
-            it 'sets the links to eq a nokogiri xml nodeset of nokogiri xml elements' do
-                stitcher = Stitchify.stitch('http://www.google.com')
-                expect(stitcher.links.class).to eq(Nokogiri::XML::NodeSet)
-                expect(stitcher.links[0].class).to eq(Nokogiri::XML::Element)
-            end
+    describe 'svg_output' do
+        it 'takes an array of arrays and returns an svg' do
+        end
+        it 'returns different values for different pixel sizes' do
+        end
+        it 'includes lines for the canvas' do
         end
     end
+
+    describe 'stitch' do
+        it 'takes an image and returns an svg' do
+        end
+    end
+    # describe 'instance method' do
+    #     describe 'get_doc' do
+    #         it 'returns a Nokogiri doc object when given a valid html string' do
+    #             stitcher = Stitchify.new()
+    #             ret = stitcher.get_doc("<html><body><h1>Hello</h1></body></html>")
+    #             expect(ret.class).to be(Nokogiri::HTML::Document)
+    #         end
+
+    #         it 'returns a Nokogiri doc object when given a valid link' do
+    #             stitcher = Stitchify.new()
+    #             ret = stitcher.get_doc("http://www.google.com")
+    #         end
+    #     end
+
+    #     describe 'build_links_array' do
+    #         before(:all) do
+    #             @html_str = "<html><body><a href='example.com'>A</a></body></html>"
+    #         end
+
+    #         it 'returns an array of arrays' do
+    #             stitcher = Stitchify.stitch(@html_str)
+    #             ret = stitcher.get_doc(@html_str)
+    #             stitcher.build_links_array
+    #             expect(stitcher.links_array.class).to eq(Array)
+    #             expect(stitcher.links_array[0].class).to eq(Array)
+    #         end
+
+    #         it 'returns A' do
+    #             stitcher = Stitchify.stitch("<html><body><a href='example.com'>A A</a></body></html>")
+    #             ret = stitcher.get_doc(@html_str)
+    #             stitcher.build_links_array
+    #             expect(stitcher.links_array).to eq([[40, 120, 40, 40], [40, 80, 80, 80], [40, 40, 80, 40], [80, 120, 80, 40], [160, 120, 160, 40], [160, 80, 200, 80], [160, 40, 200, 40], [200, 120, 200, 40]])
+    #         end
+    #     end
+
+    #     describe 'build_svg' do
+    #         before(:each) do
+    #             @html_str = "<html><body><a href='example.com'>A</a></body></html>"
+    #             @stitcher = Stitchify.stitch(@html_str)
+    #             @stitcher.build_links_array
+    #         end
+
+    #         it 'returns an svg' do
+    #             ret = @stitcher.build_svg
+    #             expect(ret.class).to eq(Rasem::SVGImage)
+    #         end
+    #     end
+    # end
+
+    # describe 'class method' do
+    #     describe 'stitch' do
+    #         it 'returns a set stitcher' do
+    #             stitcher = Stitchify.stitch('http://www.google.com')
+    #             expect(stitcher.title).to eq('Google')
+    #         end
+
+    #         it 'sets the links to eq a nokogiri xml nodeset of nokogiri xml elements' do
+    #             stitcher = Stitchify.stitch('http://www.google.com')
+    #             expect(stitcher.links.class).to eq(Nokogiri::XML::NodeSet)
+    #             expect(stitcher.links[0].class).to eq(Nokogiri::XML::Element)
+    #         end
+    #     end
+    # end
 end
 
 
